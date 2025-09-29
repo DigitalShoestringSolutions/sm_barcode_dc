f66693fa1dbe1133b038a0ad359ef901718ed7fd
Greg-Hawkridge <ghawkridge@gmail.com>
2025-09-29 10:02:05 +0100

    add new setup capabilities

--- code/setup.py
+++ code/setup.py
@@ -2,16 +2,29 @@ import logging
 import utilities.config_manager as config_manager
 from main import handle_args
 import evdev
+import json
 
 from barcode_scan import BarcodeScanner
+
+
+
+def get_input(prompt, variant="text", options=None):
+    spec = {"type": "input", "prompt": prompt, "variant": variant}
+    if options is not None:
+        spec["options"] = options
+    print(json.dumps(spec))
+    return input()
+
+def print_output(message,variant="info"):
+    print(json.dumps({"type": "output", "message": message, "variant": variant}))
 
         
 def setup(barcode_scan):
     scanner_spec = auto_detect_barcode_scanner()
     # fall back to select from list
     if scanner_spec is None:
-        print("==========================================")
-        print("The auto dectection process didn't work -\nfalling back to selecting from a list\n")
+        print_output("The auto dectection process didn't work -falling back to selecting from a list")
+        print_output("Select barcode scanner",variant="heading")
         
         scanner_spec = select_scanner()
     
@@ -25,40 +38,31 @@ def setup(barcode_scan):
 def auto_detect_barcode_scanner():
     scanner_spec = None
     while True:
-        print("==========================================")
-        print("Identifying barcode scanner:")
-        print("==========================================")
+        print_output("Identifying barcode scanner:", variant="heading")
         
-        print("If the Scanner is plugged in, please unplug it now.\n Press Enter to continue.")
-        input()
+        get_input("If the Scanner is plugged in, please unplug it now.", variant="continue")
         available_without = barcode_scan.available_devices()
-        print("Please plug it back in and press Enter to continue.")
-        input()
+        get_input("Please plug it back in.", variant="continue")
         available_with = barcode_scan.available_devices()
         
         # get differences
         for id in available_without.keys():
             del available_with[id]
         
-        
-        print("==========================================")
         if len(available_with) == 0: # no difference
-            print("No change in USB devices detected between when scanner was plugged in and unplugged")
+            print_output("No change in USB devices detected between when scanner was plugged in and unplugged", variant="error")
         elif len(available_with) == 1:
-            print("Found")
+            print_output("Found", variant="success")
             scanner_spec = list(available_with.values())[0]
             break
         else:
-            print("Multiple USB device changes detected between when scanner was plugged in and unplugged")
-        
-        print("==========================================")
-        
+             print_output("Multiple USB device changes detected between when scanner was plugged in and unplugged")
+             
         while True:
-            print("Try again? [y/n]")
-            answer = input()
+            answer = get_input("Would you like to try again? ",variant="confirm")
             if answer in ["y","Y","n","N"]:
                 break
-            print('unexpected response, please enter "y" or "n" followed by Enter')
+            print_output('unexpected response, please enter "y" or "n" followed by Enter')
         if answer in ["n","N"]:
             break
         
@@ -67,16 +71,16 @@ def auto_detect_barcode_scanner():
 def select_scanner():
     scanner_spec = None
     while True:
-        print("")
         available_list =  list(barcode_scan.available_devices().values())
+        options = {0: "Re-scan"}
         if len(available_list) == 0:
-            print("No USB devices detected...")
+            print_output("No USB devices detected...", variant="error")
         else:
             for index, entry in enumerate(available_list):
-                print(f'{index+1} - {entry["serial"]} {entry["vendor_model"]}')
+                options[index+1] = f'{entry["serial"]} {entry["vendor_model"]}'
+                # print_output(f'{index+1} - {entry["serial"]} {entry["vendor_model"]}')
         
-        print("")
-        raw_selected_index = input("Which entry is the barcode scanner that should be used?\n(enter the Number, enter 0 to re-scan): ")
+        raw_selected_index = get_input("Which entry is the barcode scanner that should be used?", variant="select", options=options)
         try:
             selected_index = int(raw_selected_index)
             if selected_index == 0:
@@ -84,14 +88,13 @@ def select_scanner():
             scanner_spec = available_list[selected_index-1]
             break
         except ValueError:
-            print("\n!! Input was not a valid number, please try again.")
+            print_output("Input was not a valid number, please try again.",variant="error")
         except IndexError:
             if len(available_list)>=1:
-                print(f"\n!! Number was too high, expected a number between 1 and {len(available_list)}.")
+                print_output(f"Number was too high, expected a number between 1 and {len(available_list)}.",variant="error")
             else:
-                print(f"\n!! No devices available to select from")
+                print_output(f"No devices available to select from",variant="error")
         
-        print("==========================================") 
         
     return scanner_spec
 
@@ -102,44 +105,39 @@ def test_scanner(barcode_scan):
     loop = asyncio.new_event_loop()
     generator =  barcode_scan.key_event_loop()
     
-    print("==========================================")
-    print("Checking barcode scanner works:")
-    print("==========================================")
-    print("Please scan a barcode to check that the scanner is working.")
-    print("All scanned barcodes will be printed below.")
+    print_output("Checking barcode scanner works", variant="heading")
+    print_output(
+        "Please scan a barcode to check that the scanner is working.\n"+
+        "All scanned barcodes will be printed below.")
     listen_task = None
     while True:
         try:
             if listen_task is None or listen_task.done():
                 listen_task = asyncio.Task(generator.__anext__(),loop=loop) 
-            print("Listening for barcode scan...")
+            print_output("Listening for barcode scan...")
             done, _pending = loop.run_until_complete(asyncio.wait([listen_task],timeout=30,return_when=asyncio.FIRST_COMPLETED))
             if listen_task in done:
                 barcode, timestamp = listen_task.result()
-                print(f"Scanner read: {barcode}")
+                print_output(f"Scanner read: {barcode}")
                 while True:
-                    print("Is this correct? [y/n]")
-                    answer = input()
+                    answer = get_input("Is this correct?",variant="confirm")
                     if answer in ["y","Y","n","N"]:
                         break
-                    print('unexpected response, please enter "y" or "n" followed by Enter')
+                    print_output('unexpected response, please enter "y" or "n" followed by Enter', variant="error")
                 if answer in ["y","Y"]:
                     break
-                print("Scan again:")
+                print_output("Scan again:")
             else:
                 current_buffer = barcode_scan.parser.current_string_buffer.getvalue()
-            print(f"Listening for barcode timed out - current buffer: {current_buffer}")
+            print_output(f"Listening for barcode timed out - current buffer: {current_buffer}",variant="error")
         except StopAsyncIteration:
-            print("An error occured when reading from the barcode scanner")
+            print_output("An error occured when reading from the barcode scanner",variant="error")
             break 
             
 
 def save_scanner_id(scanner_spec):
     # save identity to file in data volume
-    print("==========================================")
-    print("Saving Scanner ID to file:")
-    print("==========================================")
-    print("")
+    print_output("Saving Scanner ID to file", variant="heading")
     
     scanner_identity = {
         "serial": scanner_spec["serial"],
@@ -166,31 +164,21 @@ if __name__ == "__main__":
 
     barcode_scan = BarcodeScanner(conf,{})
     
-    print("")
-    print("==========================================")
-    print("Setting up Barcode Scanning Service Module")
-    print("==========================================")
-    print("")
-    print(f"Service module configured with: \n  {', \n  '.join(fingerprint)}")
-    print("")
+    print_output("Setting up Barcode Scanning Service Module", variant="heading")
+    print_output(f"Service module configured with: \n  {', \n  '.join(fingerprint)}")
     
     if barcode_scan.scanner_serial != "":
-        print("==========================================")
-        print("Barcode scanner already set up:")
-        print("==========================================")
-        print()
+        print_output("Barcode scanner already set up",variant="success")
+        
         while True:
             while True:
-                print("1 - Redo setup")
-                print("2 - Test barcode scanner")
-                print("3 - Exit")
-                print("Select an option: ")
-                raw_selected_option = input()
+                options = {1: "Redo setup", 2: "Test barcode scanner", 3: "Exit"}
+                raw_selected_option = get_input("Select an option: ", variant="select", options=options)
                 try:
                     selected_option = int(raw_selected_option)
                     break
                 except ValueError:
-                    print("\n!! Input was not a valid number, please try again.")
+                    print_output("Input was not a valid number, please try again.",variant="error")
             
             if selected_option == 1:
                 setup(barcode_scan)
@@ -202,6 +190,4 @@ if __name__ == "__main__":
     else:
         setup(barcode_scan)
         
-    print("==========================================")
-    print("Finished")
-    print("==========================================")
+    print_output("Setup complete - exiting", variant="success")
